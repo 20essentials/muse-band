8f8f2aab3cc7a95f2ff62256a4e00223db57ca54
DominicDev <s0l0estudi4r@gmail.com>
2026-01-13 13:40:52 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MUSE';
+export let nameSongs = [
+	"HYSTERIA", /*1*/
+	"UPRISING", /*2*/
+	"RESISTANCE", /*3*/
+	"UNDISCLOSED DESIRES", /*4*/
+	"MADNESS", /*5*/
+	"SUPREMACY", /*6*/
+	"SURVIVAL", /*7*/
+	"BIG FREEZE", /*8*/
+	"SAVE ME", /*9*/
+	"ISOLATED SYSTEM", /*10*/
+	"ANIMALS", /*11*/
+	"EXPLORERS", /*12*/
+	"EXO-POLITICS", /*13*/
+	"CITY OF DELUSION", /*14*/
+	"KNIGHTS OF CYDONIA", /*15*/
+	"INVINCIBLE", /*16*/
+	"GLORIOUS", /*17*/
+	"TAKE A BOW", /*18*/
+	"MAP OF THE PROBLEMATIQUE", /*19*/
+	"SHOWBIZ", /*20*/
+]
